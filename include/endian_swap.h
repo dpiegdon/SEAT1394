989391fccf4f62cf82579152525a3e5570798709
David R. Piegdon <dgit@piegdon.de>
2006-10-14 00:56:46 +0000

    and in the other functions of endian_swap, too....
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@14 e86586aa-b51f-0410-b3e5-995dd3868ebd

--- include/endian_swap.h
+++ include/endian_swap.h
@@ -6,14 +6,14 @@ inline uint64_t endian_swap64(uint64_t val)
 {
 	uint64_t sval;
 
-	((uint8_t*)&sval)[0]	= ((uint8_t*)sval)[7];
-	((uint8_t*)&sval)[1]	= ((uint8_t*)sval)[6];
-	((uint8_t*)&sval)[2]	= ((uint8_t*)sval)[5];
-	((uint8_t*)&sval)[3]	= ((uint8_t*)sval)[4];
-	((uint8_t*)&sval)[4]	= ((uint8_t*)sval)[3];
-	((uint8_t*)&sval)[5]	= ((uint8_t*)sval)[2];
-	((uint8_t*)&sval)[6]	= ((uint8_t*)sval)[1];
-	((uint8_t*)&sval)[7]	= ((uint8_t*)sval)[0];
+	((uint8_t*)&sval)[0]	= ((uint8_t*)&val)[7];
+	((uint8_t*)&sval)[1]	= ((uint8_t*)&val)[6];
+	((uint8_t*)&sval)[2]	= ((uint8_t*)&val)[5];
+	((uint8_t*)&sval)[3]	= ((uint8_t*)&val)[4];
+	((uint8_t*)&sval)[4]	= ((uint8_t*)&val)[3];
+	((uint8_t*)&sval)[5]	= ((uint8_t*)&val)[2];
+	((uint8_t*)&sval)[6]	= ((uint8_t*)&val)[1];
+	((uint8_t*)&sval)[7]	= ((uint8_t*)&val)[0];
 
 	return sval;
 }
@@ -34,8 +34,8 @@ inline uint16_t endian_swap16(uint32_t val)
 {
 	uint16_t sval;
 
-	((uint8_t*)&sval)[0]	= ((uint8_t*)sval)[3];
-	((uint8_t*)&sval)[1]	= ((uint8_t*)sval)[2];
+	((uint8_t*)&sval)[0]	= ((uint8_t*)&val)[3];
+	((uint8_t*)&sval)[1]	= ((uint8_t*)&val)[2];
 
 	return sval;
 }
