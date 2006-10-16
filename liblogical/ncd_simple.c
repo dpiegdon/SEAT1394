4d6d9dd443ff8ec02ea558fdf1825f5b10fb7dc6
David R. Piegdon <dgit@piegdon.de>
2006-10-16 04:26:05 +0000

    added simple NCD algorithm (not tested, yet) time for bed now. testing tomorrow.
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@23 e86586aa-b51f-0410-b3e5-995dd3868ebd

---
+++ liblogical/ncd_simple.c
@@ -0,0 +1,41 @@
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <bzlib.h>
+
+#define MIN(a, b)  (((a) < (b)) ? (a) : (b))
+#define MAX(a, b)  (((a) > (b)) ? (a) : (b))
+
+// simple_ncd will calculate the NCD between x,y
+float simple_ncd(void* x, size_t len_x, void* y, size_t len_y)
+{
+	void* xy;
+	void* C;
+	size_t len_C;
+	size_t Cx, Cy, Cxy;
+
+	// concat x and y to xy
+	xy = malloc(len_x + len_y);
+	memcpy(xy, x, len_x);
+	memcpy(((char*)xy)+len_x, y, len_y);
+
+	Cx = Cy = Cxy = len_C = (len_x+len_y) * 1.01 + 1000;
+	C = malloc(len_C);
+
+	// get compressed sizes for:
+	// x
+	BZ2_bzBuffToBuffCompress( C, &Cx,  x,  len_x, 9, 0, 20);
+	// y
+	BZ2_bzBuffToBuffCompress( C, &Cy,  y,  len_y, 9, 0, 20);
+	// xy
+	BZ2_bzBuffToBuffCompress( C, &Cxy, xy, len_x+len_y, 9, 0, 20);
+
+	// cleanup
+	free(xy);
+	free(C);
+
+	// calc NCD and return it.
+	return (Cxy - MIN(Cx, Cy)) / (float)MAX(Cx,Cy);
+}
+
---
+++ liblogical/ncd_simple.h
@@ -0,0 +1,10 @@
+
+#ifndef __SIMPLE_NCD_H__
+# define __SIMPLE_NCD_H__
+
+#include <stdlib.h>
+
+float simple_ncd(void* x, size_t len_x, void* y, size_t len_y)
+
+#endif // __SIMPLE_NCD_H__
+
