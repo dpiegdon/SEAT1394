44a74c7290c8bc836424321a3c1b4202cbd16069
David R. Piegdon <dgit@piegdon.de>
2006-10-17 18:36:59 +0000

    tracking error in lin->phy resolver(?)
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@36 e86586aa-b51f-0410-b3e5-995dd3868ebd

--- TESTS/linear-tester.c
+++ TESTS/linear-tester.c
@@ -14,31 +14,69 @@
 char pagedir[4096];
 
 #define PHYSICAL_DEV_MEM
-//#define MEMSOURCE "/dev/mem"
-#define MEMSOURCE "/home/datenhalde/memdump"
+#define MEMSOURCE "/dev/mem"
+//#define MEMSOURCE "/home/datenhalde/memdump"
 
 void dump_page(uint32_t pn, char* page)
 {
 	uint32_t addr;
-	int i;
+	int i,j;
+	char c;
+	printf("dump page %x\n", pn);
 
 	addr = pn * 4096;
-	for(i = 0; i<4095/32; i+=32) {
-		printf("page 0x%06x, addr 0x%08x: %2x %2x %2x %2x  %2x %2x %2x %2x  %2x %2x %2x %2x  %2x %2x %2x %2x  |  %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c\n",
-			pn, addr,
-			(page+i)[0], (page+i)[1], (page+i)[2], (page+i)[3], (page+i)[4], (page+i)[5], (page+i)[6], (page+i)[7], (page+i)[8], (page+i)[9], (page+i)[10], (page+i)[11], (page+i)[12], (page+i)[13], (page+i)[14], (page+i)[15], (page+i)[16], (page+i)[17], (page+i)[18], (page+i)[19], (page+i)[20], (page+i)[21], (page+i)[22], (page+i)[23], (page+i)[24], (page+i)[25], (page+i)[26], (page+i)[27], (page+i)[28], (page+i)[29], (page+i)[30], (page+i)[31], (page+i)[32], (page+i)[39],
-			(page+i)[0], (page+i)[1], (page+i)[2], (page+i)[3], (page+i)[4], (page+i)[5], (page+i)[6], (page+i)[7], (page+i)[8], (page+i)[9], (page+i)[10], (page+i)[11], (page+i)[12], (page+i)[13], (page+i)[14], (page+i)[15], (page+i)[16], (page+i)[17], (page+i)[18], (page+i)[19], (page+i)[20], (page+i)[21], (page+i)[22], (page+i)[23], (page+i)[24], (page+i)[25], (page+i)[26], (page+i)[27], (page+i)[28], (page+i)[29], (page+i)[30], (page+i)[31], (page+i)[32], (page+i)[39]
-		      );
+	for(i = 0; i<4096; i+=16) {
+		printf("page 0x%06x, addr 0x%08x: ", pn, addr);
+		for(j = 0; j < 16; j++) {
+			printf("%02hhx ", (page+i)[j]);
+			if((j+1)%4 == 0 && j)
+				putchar(' ');
+		}
+		printf("  |  ");
+		for(j = 0; j < 16; j++) {
+			c = (page+i)[j];
+			if(c < 0x20)
+				c = '.';
+			if(c == 0x7f)
+				c = '.';
+			putchar(c);
+//			if((j+1)%4 == 0 && j)
+//				putchar(' ');
+		}
+		printf("\n");
 	}
-
 }
 
+
+void print_linear(linear_handle h)
+{
+	addr_t pn;
+	addr_t padr;
+	char* page;
+	page = malloc(4096);
+
+	pn = 0;
+
+	while(pn < (4*1024*1024)) {
+		if(linear_to_physical(h, pn*4096, &padr))
+			printf("UNMAPPED PAGE 0x%06x\n", (uint32_t)pn);
+		else {
+			printf("page 0x%06x maps to 0x%08x\n", (uint32_t)pn, (uint32_t)padr);
+			dump_page((uint32_t)pn, page);
+		}
+		pn++;
+	}
+
+	free(page);
+}
 
 void print_stack(linear_handle h)
 {
 	static char* page;
 	addr_t pn;
 	int e;
+	addr_t upper;
+	addr_t padr;
 
 	page = malloc(4096);
 
@@ -52,30 +90,34 @@ void print_stack(linear_handle h)
 	e = -EFAULT;
 	while(e == -EFAULT) {
 		pn--;
-		printf("linear address 0x%08x\n", (uint32_t)pn*4096);
+		if(linear_to_physical(h, pn*4096, &padr))
+			printf("faulty:\n");
+		printf("seek_upper linear address 0x%08x maps to 0x%08x\n", (uint32_t)pn*4096, (uint32_t)padr);
 		e = linear_read_page(h, pn, page);
 	};
-	printf("found upper stack-bound\n");
+	printf("found upper stack-bound at page 0x%06x\n", (uint32_t)pn);
+	upper = pn+1;
 	while(e != -EFAULT) {
 		pn--;
-		printf("linear address 0x%08x\n", (uint32_t)pn*4096);
+//		printf("seek_lower linear address 0x%08x\n", (uint32_t)pn*4096);
 		e = linear_read_page(h, pn, page);
-		printf("%d\n", e);
+//		printf("%d\n", e);
 	};
-	printf("found lower stack-bound\n");
+	printf("found upper stack-bound at page 0x%06x\n", (uint32_t)pn);
 	// now we are at top-of-stack
 	// so dump it
-	while(1) {
-		if(-EFAULT == linear_read_page(h, pn, page))
-			break;
-		dump_page(pn, page);
+	while(pn<upper) {
+		if(!linear_read_page(h, pn, page)) {
+			printf("dump page %x to %x\n", (uint32_t)pn, (uint32_t)upper);
+			dump_page(pn, page);
+		}
 		pn++;
 	}
-
+	free(page);
 }
 
 
-int main(int argc, char**argv)
+int main(/*int argc, char**argv*/)
 {
 #ifdef PHYSICAL_DEV_MEM
 	int memfd;
@@ -136,7 +178,9 @@ int main(int argc, char**argv)
 					printf("loading pagedir failed\n");
 					continue;
 				}
-				print_stack(lin);
+				//print_stack(lin);
+				print_linear(lin);
+				return 0;
 			}
 		}
 	}
