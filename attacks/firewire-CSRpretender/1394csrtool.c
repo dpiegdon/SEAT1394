d74fe7f5b4400f0bfb7a4f6fd47bbaab42a1154c
David R. Piegdon <dgit@piegdon.de>
2006-10-25 00:47:26 +0000

    sync after system crash: 1394csrtool stuff
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@49 e86586aa-b51f-0410-b3e5-995dd3868ebd

--- attacks/firewire-CSRpretender/1394csrtool.c
+++ attacks/firewire-CSRpretender/1394csrtool.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,34 +12,153 @@
 #include <libraw1394/raw1394.h>
 #include <libraw1394/csr.h>
 
-#define MAX_PORTS	20
+#define MAX_PORTS	100
+#define NODE_OFFSET	0xffbf
+#define BUFFERSIZE	8192
+#define CONFIGROMSIZE	1024
+
+static char buffer[BUFFERSIZE];
 
 int csr_read_to_file(int port, char* filename)
 {
 	raw1394handle_t h;				// handle to access raw1394
+	size_t romsize;
+	unsigned char version;
+	int fd;
+	int r;
 
-	h = raw1394_new_handle();
+	// open handle
+	h = raw1394_new_handle_on_port(port);
+	if(!h) {
+		printf("failed to open raw1394 or port %d\n", port);
+		return -1;
+	}
+
+	// blank buffer
+	memset(buffer, 0, CONFIGROMSIZE);
+	// get config rom
+	if( (r = raw1394_get_config_rom(h, (quadlet_t*) buffer, CONFIGROMSIZE, &romsize, &version)) ) {
+		printf("failed to read config rom (buffer too small?) (%d)\n", r);
+		return -1;
+	} else {
+		printf("read config rom: size: %d, version: %d\n", romsize, version);
+
+		// open dumpfile and dump the config rom
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if(fd < 0) {
+			printf("failed to open dump \"%s\"\n", filename);
+			return -1;
+		}
+		if(romsize != write(fd, buffer, CONFIGROMSIZE)) {
+			close(fd);
+			printf("failed to write to dump \"%s\"\n", filename);
+			return -1;
+		}
+		close(fd);
+		printf("dumped to \"%s\"\n", filename);
+	}
+
+	return 0;
 }
 
 int csr_write_from_file(int port, char* filename)
 {
 	raw1394handle_t h;				// handle to access raw1394
+	size_t romsize;
+	unsigned char version;
+	int fd;
+	int r;
 
-	h = raw1394_new_handle();
+	h = raw1394_new_handle_on_port(port);
+	if(!h) {
+		printf("failed to open raw1394 or port %d\n", port);
+		return -1;
+	}
+
+	// read old config rom. needed to get version
+	if( raw1394_get_config_rom(h, (quadlet_t*) buffer, CONFIGROMSIZE, &romsize, &version) ) {
+		printf("failed to read old config rom (buffer to small?)\n");
+		return -1;
+	} else {
+		printf("read old config rom: version: %d\n", version);
+
+		// blank buffer
+		memset(buffer, 0, CONFIGROMSIZE);
+
+		// read new configrom from dump
+		fd = open(filename, O_RDONLY);
+		if(fd < 0) {
+			printf("failed to open dump \"%s\"\n", filename);
+			return -1;
+		}
+		romsize = read(fd, buffer, CONFIGROMSIZE);
+		if(romsize <= 0) {
+			close(fd);
+			printf("failed to read new config rom from \"%s\"\n", filename);
+			return -1;
+		}
+		close(fd);
+		printf("read new config rom from \"%s\", size %d\n", filename, romsize);
+
+		// set new config rom
+		if((r = raw1394_update_config_rom(h, (quadlet_t*) buffer, CONFIGROMSIZE, version))) {
+			printf("failed to set new config rom. reason: %s\n",
+					(r == -1) ? "version is incorrect" :
+						((r == -2) ? "new rom version is too big" : "unknown"));
+			return -1;
+		}
+	}
+
+	return 0;
+
 }
 
 int csr_dump(int port, nodeid_t target, char* filename)
 {
 	raw1394handle_t h;				// handle to access raw1394
+	uint64_t config_adr;
+	char* buffer_adr;
+	size_t romsize;
+	unsigned char version;
+	int r;
 	int p;
+	int fd;
 
-	h = raw1394_new_handle();
-	p = raw1394_set_port(h, port);
-	if(p != 0)
-		printf("port %d failed", port);
-		goto release;
+	h = raw1394_new_handle_on_port(port);
+	if(!h) {
+		printf("failed to open raw1394 or port %d\n", port);
+		return -1;
 	}
+	
+#define CHUNKSIZE 128
 
+	// read CSR_CONFIG_ROM ... CSR_CONFIG_ROM_END from target
+	
+	for(r = 0; r < ((CSR_CONFIG_ROM - CSR_CONFIG_ROM_END) / CHUNKSIZE); r++) {
+		config_adr = CSR_CONFIG_ROM + CHUNKSIZE * r;
+		buffer_adr = buffer + CHUNKSIZE * r;
+printf("dumping adr 0x%llx, size %x to %p\n", config_adr, CHUNKSIZE, buffer_adr);
+
+		p = raw1394_read(h, target + NODE_OFFSET, config_adr, CHUNKSIZE, (quadlet_t*) buffer_adr);
+		if(p) {
+			printf("failed to read target %d (0x%04x) at rom offset 0x%04x\n", target, target + NODE_OFFSET, config_adr);
+		}
+	}
+	printf("read config rom of port %d, target %d (0x%04x)\n", port, target, target + NODE_OFFSET);
+
+	// open dumpfile and dump the config rom
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if(fd < 0) {
+		printf("failed to open dump \"%s\"\n", filename);
+		return -1;
+	}
+	if(romsize != write(fd, buffer, CONFIGROMSIZE)) {
+		close(fd);
+		printf("failed to write to dump \"%s\"\n", filename);
+		return -1;
+	}
+	close(fd);
+	printf("dumped to \"%s\"\n", filename);
 }
 
 int ieee1394scan()
@@ -47,9 +166,16 @@ int ieee1394scan()
 	raw1394handle_t h;				// handle to access raw1394
 	struct raw1394_portinfo pinf[MAX_PORTS];	// array of all available ports
 	nodeid_t self;					// nodeid of self
+	nodeid_t somenode;
 	int p;
+	int i;
 
 	h = raw1394_new_handle();
+	if(!h) {
+		printf("failed to open raw1394\n");
+		return -1;
+	}
+
 	p = raw1394_get_port_info(h, pinf, MAX_PORTS);
 	printf("got %d ieee1394 ports:\n", p);
 	// print info on all ports
@@ -80,7 +206,7 @@ void usage(char* progname)
 	       "\t -s : scan all ports\n");
 }
 
-union command {
+enum command {
 	COMMAND_NONE,
 	COMMAND_READ,
 	COMMAND_WRITE,
@@ -93,13 +219,13 @@ int main(int argc, char**argv)
 	int c;						// char for getopt
 
 	// arguments:
-	union command cmd = COMMAND_NONE;
+	enum command cmd = COMMAND_NONE;
 	int port = -1;
-	nodeid_t target = -1;
+	nodeid_t target = 0;
 	char* filename = NULL;
 
 	// parse options
-	while( -1 != (c = getopt(argc, argv, "rwdsp:b:f:")) ) {
+	while( -1 != (c = getopt(argc, argv, "rwdsp:t:f:")) ) {
 		switch (c) {
 			case 'r':
 				if(cmd != COMMAND_NONE) {
@@ -136,7 +262,7 @@ int main(int argc, char**argv)
 			case 'p':
 				port = atoi(optarg);
 				break;
-			case 'b':
+			case 't':
 				target = atoi(optarg);
 				break;
 			case 'f':
@@ -152,28 +278,28 @@ int main(int argc, char**argv)
 			return -1;
 			break;
 		case COMMAND_READ:
-			if( (port != -1) || (target != -1) || (filename == NULL) ) {
+			if( (port == -1) || (target) || (!filename) ) {
 				printf("error: -r does not use -t and requires -p and -f\n");
 				return -1;
 			}
 			return csr_read_to_file(port, filename);
 			break;
 		case COMMAND_WRITE:
-			if( (port != -1) || (target != -1) || (filename == NULL) ) {
+			if( (port == -1) || (target) || (!filename) ) {
 				printf("error: -w does not use -t and requires -p and -f\n");
 				return -1;
 			}
 			return csr_write_from_file(port, filename);
 			break;
 		case COMMAND_DUMP:
-			if( (port == -1) || (target == -1) || (filename == NULL)) {
+			if( (port == -1) || (!target) || (!filename)) {
 				printf("error: -w requires -p and -t and -f\n");
 				return -1;
 			}
 			return csr_dump(port, target, filename);
 			break;
 		case COMMAND_SCAN:
-			if( (port != -1) || (target != -1) || (filename != NULL)) {
+			if( (port != -1) || (target) || (filename)) {
 				printf("error: -s does not require any parameters\n");
 				return -1;
 			}
