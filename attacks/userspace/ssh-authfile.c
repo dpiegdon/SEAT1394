0e44d69798360f926db860f66ed14d989e027b96
David R. Piegdon <dgit@piegdon.de>
2006-10-30 02:01:41 +0000

    more
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@89 e86586aa-b51f-0410-b3e5-995dd3868ebd

---
+++ attacks/userspace/ssh-authfile.c
@@ -0,0 +1,12 @@
+/*  $Id$
+ *  authfile stuff, mostly stolen from openssh 4.3p2
+ *  for license, see openssh-source or openssh.org
+ */
+
+// from authfile.c :
+
+int key_save_private(Key *key, const char *filename, const char *passphrase, const char *comment)
+{
+
+}
+
