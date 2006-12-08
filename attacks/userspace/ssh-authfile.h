13eeea03c8dcaab689db746bc885f4bee814a0b3
David R. Piegdon <dgit@piegdon.de>
2006-12-08 05:43:28 +0000

    cleanup
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@203 e86586aa-b51f-0410-b3e5-995dd3868ebd

--- attacks/userspace/ssh-authfile.h
+++ attacks/userspace/ssh-authfile.h
@@ -1,15 +1,13 @@
 /*  $Id$
  *  sshkey-structs
- *  stolen from key.h and authfile.h from openssh
+ *  stolen from key.h
  */
 
 #ifndef __SSHKEY_STRUCTS_H__
 # define __SSHKEY_STRUCTS_H__
 
-#include <openssl/evp.h>
 #include <openssl/rsa.h>
 #include <openssl/dsa.h>
-//#include <openssl/evp.h>
 
 // from key.h:
 
@@ -25,11 +23,6 @@ typedef struct Key {
         RSA     *rsa;
         DSA     *dsa;
 } Key;
-
-// from authfile.h:
-
-//int key_save_private(Key *key, const char *filename, const char *passphrase, const char *comment);
-Key * key_load_private(const char *filename, const char *passphrase, char **commentp);
 
 #endif // __SSHKEY_STRUCTS_H__
 
