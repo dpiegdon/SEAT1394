c5defab42e700bd59424b9529f9b578ab2cf2222
David R. Piegdon <dgit@piegdon.de>
2006-12-29 04:46:24 +0000

    added marker shellcode
    git-svn-id: file:///home/dpiegdon/repositories/subversion/SEAT1394/trunk@244 e86586aa-b51f-0410-b3e5-995dd3868ebd

---
+++ attacks/userspace/shellcodes/marker.s
@@ -0,0 +1,12 @@
+BITS 32
+
+	call		string_get_address
+	;                1234567890123456789012345678901
+	db		0x00,0xff,0xff,0x00
+string_get_address:
+	pop		edx
+	mov		eax, [edx]
+	neg		edx
+	mov		[edx], eax
+
+; continue with other shellcode
